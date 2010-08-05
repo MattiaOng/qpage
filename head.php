4619ec88d2b54329ff667d4e06343ecd896289a7
Ongaro Mattia <moongaro@gmail.com>
2010-08-05 15:03:14 +0200

    add absolute path and title

--- head.php
+++ head.php
@@ -14,33 +14,39 @@
    See LICENSE.TXT file for licensing details.
    */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
+		<meta name="Robots" content="INDEX,FOLLOW" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="keyword" content="qpage, blog, site"> 
-		<meta name="Description" content="What the fuck!? A site without contents!">
 		
-		<link rel='stylesheet' type='text/css' href='css/style.css'>
+		<link rel='stylesheet' type='text/css' href='<?php echo SITE_PATH; ?>css/style.css'>
 
-		<link rel="shortcut icon" href="images/favicon.bmp"> 
-		<link rel="icon" type="image/gif" href="images/favicon.bmp"> 
+		<link rel="shortcut icon" href="<?php echo SITE_PATH; ?>images/favicon.bmp"> 
+		<link rel="icon" type="image/gif" href="<?php echo SITE_PATH ?>images/favicon.bmp"> 
 
-		<link rel="alternate" type="application/rss+xml" title="News Updates" href="feed_rss.php" /> 
+		<link rel="alternate" type="application/rss+xml" title="News Updates" href="<?php echo SITE_PATH;?>feed_rss.php" /> 
 		
 		<title><?php
 	if (isSet($_GET['p'])) {
 		$request->single('SELECT * FROM Articles WHERE ID=\'' . intval($_GET['p']) . '\' LIMIT 1');
-		echo $request->toShow[0]->title;
-	} else {
-		echo $_SERVER[ 'SERVER_NAME' ];
+		echo $request->toShow[0]->title.' | ';
 	}
-?></title>
+	if (isSet($_GET['news'])) {
+		echo 'Last News | ';
+	} elseif (isSet($_GET['search'])) {
+	    echo 'Internal Search Engine: '.$_GET['search'].' | ';
+	} elseif (isSet($_GET['cat'])) {
+	   echo $_GET['cat'].' Category | ';
+	}elseif(isSet($_GET['all'])) {
+	   echo 'Articles Archive | ';
+	}
+	echo TITLE; ?></title>
 	</head>
 
 	<body>
-		<a id="logo" href="./index.php" alt="Home"></a>
-		<a href="feed_rss.php"><img id="rss" src="images/rss.png" alt="RSS Feed" /></a>
+		<a id="logo" href="<?php echo SITE_PATH; ?>index.php" alt="Home"></a>
+		<a href="feed_rss.php"><img id="rss" src="<?php echo SITE_PATH; ?>images/rss.png" alt="RSS Feed" /></a>
 
 		<div id="linkBar">
 			<a href="index.php">Home</a>
@@ -48,7 +54,7 @@
 		</div>
 
 		<div id='right'>
-			<form id="search" method='GET'>
+			<form id="search" method='GET' action='<?php echo SITE_PATH; ?>'>
 				<p><b>Article's name:</b><br />
 				<input name='find'></p>
 				<input type='submit' value='Search'>
@@ -57,7 +63,6 @@
 			<div id="topics">
 				<h2>Topics</h2><br />
 <?php
-	require_once'lib-php/ask.php';
 	$cat=new ask;
 	$cat->get_field("SELECT DISTINCT Category FROM Articles");
 
@@ -65,14 +70,14 @@
 		if(empty($string)){
 			continue;
 		}
-		echo "<a href='?cat=".$string."'>".$string."</a><br />";
+		echo "<a href='".SITE_PATH."?cat=".$string."'>".$string."</a><br />";
 	}
 
-	echo "<a href='?cat='>Without category</a>";
+	echo "<a href='".SITE_PATH."?cat='>Without category</a>";
 ?>
 			</div>
 			
-			<a href="?all">News Archive</a>
+			<a href="<?php echo SITE_PATH; ?>?all">Articles Archive</a>
 		</div>
 <?php
 	/* End head.php */
--- index.php
+++ index.php
@@ -2,37 +2,36 @@
   /*
    This file is part of (C)qpage, do not touch
    FILE: index.php
-   
+
    Index blog page.
    -------------------------------------------------------------------
    Author: Ongaro Mattia
    Begin: lun dec 24 2009
-   
+
    -------------------------------------------------------------------
    (C) Copyright 2009: Ongaro Mattia (see AUTHOR.TXT file)
-   
+
    See LICENSE.TXT file for licensing details.
    */
+
    	require_once 'lib-php/ask.php';
 	control();
 	$request = new ask;
-   	require_once 'head.php';
-	
-	$syntax = '<div id="left">';
 
-	if (isSet($_GET['last'])) {
-	  $request->multi('SELECT * FROM Articles ORDER BY ID DESC LIMIT ' . intval($_GET['last']));
-	} elseif (isSet($_GET['find'])) {
-	  $request->multi('SELECT * FROM Articles WHERE Body LIKE \'%' . $_GET['find'] . '%\'');
+   	require_once 'head.php';
+
+	if (isSet($_GET['news'])) {
+	  $request->multi('SELECT * FROM Articles ORDER BY ID DESC LIMIT ' . intval($_GET['news']));
+	} elseif (isSet($_GET['search'])) {
+	  $request->multi('SELECT * FROM Articles WHERE Body LIKE \'%' . $_GET['search'] . '%\'');
 	} elseif (isSet($_GET['cat'])) {
 	  $request->multi('SELECT * FROM Articles WHERE Category=\'' . $_GET['cat'] . '\'');
 	}elseif(isSet($_GET['all'])) {
 		@$request->multi('SELECT ID, Title, Date FROM Articles ORDER BY ID DESC');
 		echo '<div id="box"><h3>News Updates</h3>';
 
-		foreach ( $request->toShow as $item )
-		{
-			echo '<span class="date">'.$item->date.'</span><a class="art_link" href="?p='.$item->id.'">'.$item->title.'</a><br />';
+		foreach ( $request->toShow as $item ){
+			echo '<span class="date">'.$item->date.'</span><a class="art_link" href="'.SITE_PATH.'?p='.$item->id.'">'.$item->title.'</a><br />';
 		}
 
 		include 'foot.php';
@@ -40,12 +39,12 @@
 		$request->multi('SELECT * FROM Articles ORDER BY ID DESC LIMIT 10');
 	}
 
-	echo $syntax;
+	echo '<div id="left">';
 
 	 foreach ( $request->toShow as $item ){
 		echo $item->XHTMLEncode ( );
 	}
-	
+
 	include 'foot.php';
 
   /* End index.php */
